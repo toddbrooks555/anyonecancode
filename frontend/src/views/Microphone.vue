b7585842be4680e81e7e9d46bc24f6d36dfc9bcb
Todd Brooks <136504567+toddbrooks555@users.noreply.github.com>
2023-06-14 15:05:38 -0400

    Update Microphone.vue
    Added French and UK English

--- frontend/src/views/Microphone.vue
+++ frontend/src/views/Microphone.vue
@@ -3,6 +3,8 @@
     <div id="select">
       <h2>Select your language:</h2>
       <select name="lang" @change="onChange($event)" class="custom-select">
+        <option value="fr-FR" selected>Francais</option>
+        <option value="en-GB" selected>English UK</option>
         <option value="de-DE" selected>German</option>
         <option value="en-US">English</option>
       </select>
